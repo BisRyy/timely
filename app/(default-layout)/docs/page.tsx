05182791d4cfe290eb84ccf537a80fcf587dd5eb
Bisrat Kebere <kiyyakebere@gmail.com>
2024-02-28 09:05:07 +0900

    Add User Manual Documentation

--- app/(default-layout)/docs/page.tsx
+++ app/(default-layout)/docs/page.tsx
@@ -1,8 +1,10 @@
-export default function Docs() {
-    return (
-        <div>
-            <h1 className="text-3xl font-semibold tracking-tight">Documentation</h1>
-            <p className="text-zinc-800 text-sm"><span className="text-red-500">Coming Soon</span></p>
-        </div>
-    )
-}
+import React from 'react';
+const GoogleDocumentViewer = () => {
+  const documentURL = 'https://docs.google.com/document/d/1wyD5pFXpIl0kVHvaZyFnDELNIHgUHLQpRKP66sEOwrg/edit?usp=sharing';
+  return (
+    <div>
+      <iframe src={documentURL} width="100%" height="600px" />
+    </div>
+  );
+};
+export default GoogleDocumentViewer;
